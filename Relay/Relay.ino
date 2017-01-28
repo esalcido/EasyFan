db9df60aba707f9e7984e7108bb37de0aa64ef16
esalcido <edward.salcido@gmail.com>
2017-01-28 12:33:51 -0800

    adding arduino code

---
+++ Relay/Relay.ino
@@ -0,0 +1,35 @@
+
+#define RELAY1 7
+byte byteRead;
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+
+pinMode(RELAY1, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+if(Serial.available() ){
+  byteRead = Serial.read();
+
+  if(byteRead == 49){
+
+    digitalWrite(RELAY1, 0);
+  Serial.println("Light On");
+  
+  }
+  if(byteRead == 48){
+     digitalWrite(RELAY1,1);
+  Serial.println("Light off");
+  
+  }
+}
+  
+
+ 
+  
+  
+}
